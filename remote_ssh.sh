dabcd642de974cdebefd309d3724f02281ddb4d5
Youngrok Cho <youngrch@c3r7s4.42seoul.kr>
2021-03-11 05:55:17 +0900

    last

--- remote_ssh.sh
+++ remote_ssh.sh
@@ -1,1 +1,1 @@
-ssh -o StrictHostKeyChecking=no -i ~/remote_ssh/mykey -fNR 4321:localhost:8080 ubuntu@193.123.238.90
+ssh -o StrictHostKeyChecking=no -i ~/remote_ssh/mykey -fNR 4321:localhost:8080 ubuntu@000.000.000.00
---
+++ ubuntu_connect.sh
@@ -0,0 +1,2 @@
+chmod 400 ~/remote_ssh/mykey
+ssh -o StrictHostKeyChecking=no -i ~/remote_ssh/mykey ubuntu@000.000.000.00
