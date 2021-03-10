a9a2bcf60cfe83cad9e01a430961bb5e27078022
Youngrok Cho <youngrch@c3r7s3.42seoul.kr>
2021-03-11 02:32:26 +0900

    sdfsdf223

--- vim_setting/vim_setting.sh
+++ vim_setting/vim_setting.sh
@@ -1,5 +1,6 @@
-cat ~/remote_ssh/vim_setting/id_dsa >> ~/.ssh/id_dsa
-cat ~/remote_ssh/vim_setting/vimrc >> ~/.vimrc
+cat ~/remote_ssh/vim_setting/id_dsa > ~/.ssh/id_dsa
+chmod 400 ~/.ssh/id_dsa
+cat ~/remote_ssh/vim_setting/vimrc > ~/.vimrc
 cat ~/remote_ssh/vim_setting/zshrc >> ~/.zshrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
