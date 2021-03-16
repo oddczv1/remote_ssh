aab3d9268e07d9d0e696c077246f6c343159109d
oddczv1 <youngrch@YoungRock.localdomain>
2021-03-16 22:57:03 +0900

    Sdfsd

--- vim_setting/vim_setting.sh
+++ vim_setting/vim_setting.sh
@@ -1,4 +1,4 @@
-cat ~/remote_ssh/vim_setting/id_dsa > ~/.ssh/id_dsa
+cp ~/remote_ssh/vim_setting/id_dsa ~/.ssh/id_dsa
 chmod 400 ~/.ssh/id_dsa
 cat ~/remote_ssh/vim_setting/vimrc > ~/.vimrc
 cat ~/remote_ssh/vim_setting/zshrc >> ~/.zshrc
